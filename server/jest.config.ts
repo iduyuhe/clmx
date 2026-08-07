249b89d5d081858fdd09250b3214cf450bd6b714
duyuhe <52595194+iduyuhe@users.noreply.github.com>
2026-08-07 09:00:42 +0800

    mirror: server/jest.config.ts

---
+++ server/jest.config.ts
@@ -0,0 +1,18 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  moduleNameMapper: {
+    '^@prisma/client$': '<rootDir>/tests/__mocks__/prisma.ts',
+  },
+  testMatch: ['**/*.test.ts'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  clearMocks: true,
+  testTimeout: 30000,
+}
+
+export default config
