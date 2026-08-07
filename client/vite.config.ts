f25f959a1cd03585273ece6e75ac86bc094801b2
duyuhe <52595194+iduyuhe@users.noreply.github.com>
2026-08-07 09:00:04 +0800

    mirror: client/vite.config.ts

---
+++ client/vite.config.ts
@@ -0,0 +1,28 @@
+/// <reference types="vitest/config" />
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+export default defineConfig({
+  base: './',
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:3002',
+        changeOrigin: true,
+      },
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+    css: true,
+  },
+});
